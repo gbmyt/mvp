16c87a4317d2a6f215d0943423b9eb32704a42e2
gbmyt <gbmytabc1@gmail.com>
2022-12-13 23:28:48 -0500

    config files updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
-const path = require('path');
+const path = require("path");
 
-var DIST_DIR = path.resolve(__dirname, 'client/dist');
+var DIST_DIR = path.resolve(__dirname, "client/dist");
 
 module.exports = {
-	mode: 'development',
-	entry: './index.js',
+  mode: "development",
+  entry: "./index.js",
 
-	output: {
-		path: DIST_DIR,
-		filename: 'bundle.js'
-	},
+  output: {
+    path: DIST_DIR,
+    filename: "bundle.js",
+  },
 
-	devServer: {
-		port: 3000,
-		static: [path.resolve(__dirname, 'client')],
-		open: true
-	},
+  devServer: {
+    port: 3000,
+    static: [path.resolve(__dirname, "client")],
+    open: true,
+    hot: true,
+    liveReload: false,
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: 'babel-loader'
-			}
-		]
-	}
-}
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
