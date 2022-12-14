35ddd1c93375076c67aeb77e35ac8c6abf6d6740
gbmyt <gbmytabc1@gmail.com>
2022-12-13 22:41:43 -0500

    Added babel and webpack config files

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+var DIST_DIR = path.resolve(__dirname, 'client/dist');
+
+module.exports = {
+	mode: 'development',
+	entry: './index.js',
+
+	output: {
+		path: DIST_DIR,
+		filename: 'bundle.js'
+	},
+
+	devServer: {
+		port: 3000,
+		static: [path.resolve(__dirname, 'client')],
+		open: true
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			}
+		]
+	}
+}
