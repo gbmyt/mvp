4ef6c953f4c329e919a1acc1143f1d41027fcb75
gbmyt <gbmytabc1@gmail.com>
2022-12-14 00:36:49 -0500

    Added Express

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-// Import Express
 var express = require("express");
 var app = express();
 var port = 3000;
 
-// Set up some route handlers and middleware functions
+var getYogaPoses = require('../helpers/poses');
+
 app.use('/', express.static("client"));
 
-app.get('/', (req, res) => {
-  console.log("Testing Express install");Ï
+app.get('/poses', (req, res) => {
+	var poses = getYogaPoses();
+  console.log("Poses", poses);
+	res.send(res.statusCode);
 });
-
-// app.post()
 
 app.listen(port, (err) => {
   if (err) console.log(err);
